bcd15503edc5eb182b056847e793d2b153d7ca50
saman <saman@samans-MBP.attlocal.net>
2022-07-03 18:15:23 -0500

    http server

--- app.js
+++ app.js
@@ -1,56 +1,24 @@
-import 'dotenv/config';
-
-
-
-console.log(process.env.ENV_VAR_0);
-
-
-
-
-// ===========================================================================================
+import 'dotenv/config'; // necessary for env value be read...
+import http, { Server } from 'http';
 
 
 
 
+const port = 3000 || process.env.PORT;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-type', 'text/html');
+    res.write('Hello World!'); //write a response to the client
+    // res.write('<h1>Hello, world!</h1>'); 
+    res.end(); //end the response
+});
 
 
 
-// ===========================================================================================
-
-
-
-
-
-
-
-// ===========================================================================================
-
-
-
-
-
-
-
-// ===========================================================================================
-
-
-
-
-
-
-
-// ===========================================================================================
-
-
-
-
-
-
-
-// ===========================================================================================
-
-
-
+server.listen(port, () => {
+    console.log(`server running at port ${port}`);
+})
 
 
 
